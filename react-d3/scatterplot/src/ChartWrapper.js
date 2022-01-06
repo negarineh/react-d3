e7a7a32befad2edd76fb92299ce3f125a27c3cc1
Nafis <27172694+negarineh@users.noreply.github.com>
2022-01-06 15:06:48 +1100

    Added files

---
+++ react-d3/scatterplot/src/ChartWrapper.js
@@ -0,0 +1,22 @@
+import React, { useRef, useState, useEffect } from 'react';
+import D3Chart from './D3Chart';
+
+const ChartWrapper = ({ data, updateName }) => {
+	const chartArea = useRef(null)
+	const [chart, setChart] = useState(null)
+
+	useEffect(() => {
+		if (!chart) {
+			setChart(new D3Chart(chartArea.current, data, updateName))
+		}
+		else {
+			chart.update(data)
+		}
+	}, [chart, data, updateName])
+
+	return (
+		<div className="chart-area" ref={chartArea}></div>
+	)
+}
+
+export default ChartWrapper
